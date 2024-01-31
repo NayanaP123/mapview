42ec5fc830ca665c492a43896232f13ce7a81ee6
ayanaP123 <nayanarajeendran2001@gmail.com>
2024-01-31 11:55:25 +0530

    first commit

---
+++ map/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4016584.092792871!2d76.13836674999999!3d10.544276350000011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0812ffd49cf55b%3A0x64bd90fbed387c99!2sKerala!5e0!3m2!1sen!2sin!4v1706516077073!5m2!1sen!2sin" width="600" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  );
+}
+
+export default App;
